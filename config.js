4f4844d4895d6abe1088aeb03e79366f4381dfa6
dilusha111 <ndilusha003@gmail.com>
2024-12-21 20:22:06 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
 SESSION_ID: 'HANSAMAL-MD=LR0GkbYY#DhTbcObfV_m3r-Tpw-3_o-4onlhaKebOLXylE12WH6c',
-GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? 'YOUR GIT TOKEN (gph_without)' : process.env.GITHUB_AUTH_TOKEN,
-GITHUB_USER_NAME: process.env.GITHUB_USER_NAME === undefined ? 'YOUR GIT USER NAME' : process.env.GITHUB_USER_NAME,
-OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? 'YOUR NUMBER' : process.env.OWNER_NUMBER
+GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? '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' : process.env.GITHUB_AUTH_TOKEN,
+GITHUB_USER_NAME: process.env.GITHUB_USER_NAME === undefined ? 'dilusha111' : process.env.GITHUB_USER_NAME,
+OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94760334624' : process.env.OWNER_NUMBER
 };
